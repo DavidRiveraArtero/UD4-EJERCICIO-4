8f4af1feda62567a0a6d84c9329520eedc557a2e
DavidRiveraArtero <davidtoy222@gmail.com>
2021-07-07 10:36:25 +0200

    UD4-Ej4

---
+++ UD4_OperacionesEj4App.java
@@ -0,0 +1,24 @@
+
+public class UD4_OperacionesEj4App {
+
+	public static void main(String[] args) {
+		
+		int n = 10;
+		
+		//VALOR INICIAL
+		System.out.println("VALOR INICIAL: " + n);
+		
+		//VALOR SUMANDOLE 77
+		n = n + 77;
+		System.out.println("VALOR SUMANDO 77: " + n);
+		
+		//VALOR RESTANDOLE 3
+		n = n - 3;
+		System.out.println("VALOR RESTANDO 3" + n);
+		
+		//DUPLICAR SU VALOR 
+		n = n * 2;
+		System.out.println("VALOR DUPLICANDO SU VALOR " + n);
+	}
+
+}
