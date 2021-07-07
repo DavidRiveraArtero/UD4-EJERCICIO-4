c0d41ac7fb32b5f074d347413ff0f5dcf728d40f
DavidRiveraArtero <87012638+DavidRiveraArtero@users.noreply.github.com>
2021-07-07 11:08:42 +0200

    Update UD4_OperacionesEj4App.java

--- UD4_OperacionesEj4App.java
+++ UD4_OperacionesEj4App.java
@@ -14,7 +14,7 @@ public class UD4_OperacionesEj4App {
 		
 		//VALOR RESTANDOLE 3
 		n = n - 3;
-		System.out.println("VALOR RESTANDO 3" + n);
+		System.out.println("VALOR RESTANDO 3: " + n);
 		
 		//DUPLICAR SU VALOR 
 		n = n * 2;
